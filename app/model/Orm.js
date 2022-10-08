37cee023687737714c6fc7c51dd9c74e62707645
GhosTHaise <Ghostrex2@gmail.com>
2022-10-08 20:21:49 +0300

    auto have table name from orm instance

--- app/model/Orm.js
+++ app/model/Orm.js
@@ -1,53 +1,8 @@
 const cnx = require("./db");
 
-const select = async(colonne,table,others) => {
-
-    return new Promise((resolve,reject)=>{
-        cnx.query(`SELECT ${String(colonne)} FROM ${table} ${others} `,(err,data)=>{
-            //console.log(data);
-            if(err){
-                reject(err)
-            }else{
-               resolve(data)
-            }
-        });
-    })
-}
-
-const insert = async(table,data) => {
-   return new Promise((resolve,reject)=>{
-        cnx.query(`
-            INSERT INTO ${table} (${String(Object.keys(data))})
-            VALUES (${"?,".repeat((Object.values(data).length) - 1)}?)
-        `,Object.values(data),(err,_) => {
-            if(err){
-                reject(err)
-            }else{
-                resolve("Les informations ont bien ete enregistre dans la table : "+table);
-            }
-        });
-   })
-}
-
-const remove = async(table,others) => {
-    return new Promise((resolve,reject) =>{
-        cnx.query(`DELETE FROM ${table} WHERE ${others}`,(err,_) => {
-            if(err){
-                resolve({
-                    message : "Les donnes ont bien ete supprimer."
-                })
-            }else{
-                reject({
-                    message : "Impossible de supprimer les donnees."
-                })
-            }
-        });
-    })
-}
-
 const update = async(table,values,other) => {
     return new Promise((resolve,reject)=>{
-        cnx.query(`UPDATE ${table} SET ${values} WHERE ${other}`,(err,_)=>{
+        cnx.query(`UPDATE ${this.table_name} SET ${values} WHERE ${other}`,(err,_)=>{
             if(err){
                 resolve({
                     message : "Les donnes ont bien ete mis a jour."
@@ -63,13 +18,76 @@ const update = async(table,values,other) => {
 
 class ORM{
     //creation d'instance
+    constructor(model){
+        this.table_name = model
+    }
     new(){
         return this;
     };
     //methode propre
-    supprimer = remove
-    inserer = insert
-    recuperer = select
+    //creation d'instance
+    constructor(name){
+        this.table_name = name
+    }
+    new = () =>{
+        return this
+    }
+    //method propre
+    supprimer = async(others) => {
+        return new Promise((resolve,reject) =>{
+            cnx.query(`DELETE FROM ${this.table_name} WHERE ${others}`,(err,_) => {
+                if(err){
+                    reject({
+                        message : "Impossible de supprimer les donnees."
+                    })
+                }else{
+                    resolve({
+                        message : "Les donnes ont bien ete supprimer."
+                    })
+                }
+            });
+        })
+    }
+    inserer = async(table,data) => {
+        return new Promise((resolve,reject)=>{
+             cnx.query(`
+                 INSERT INTO ${this.table_name} (${String(Object.keys(data))})
+                 VALUES (?,?)
+             `,Object.values(data),(err,_) => {
+                 if(err){
+                     reject(err)
+                 }else{
+                     resolve("Les informations ont bien ete enregistre dans la table : "+table);
+                 }
+             });
+        })
+     }
+    recuperer = async(colonne,others) => {
+        return new Promise((resolve,reject)=>{
+            cnx.query(`SELECT ${String(colonne)} FROM ${this.table_name} ${others} `,(err,data,fields)=>{
+                //console.log(data);
+                if(err){
+                    reject(err)
+                }else{
+                //console.log("data : ",data,"fields :",fields);
+                   resolve(data)
+                }
+            });
+        })
+    }
+    recuperer = async(colonne,table,others) => {
+        return new Promise((resolve,reject)=>{
+            cnx.query(`SELECT ${String(colonne)} FROM ${this.table_name} ${others} `,(err,data,fields)=>{
+                //console.log(data);
+                if(err){
+                    reject(err)
+                }else{
+                //console.log("data : ",data,"fields :",fields);
+                   resolve(data)
+                }
+            });
+        })
+    }
     mettreAjour = update
 }
 
